0c2002919a2960f7651a4bff287c17770b18ef65
oxxo <oxxo.studio@gmail.com>
2019-03-07 23:23:01 +0800

    edit msg

--- blockly/msg/blocks/zh-hant.js
+++ blockly/msg/blocks/zh-hant.js
@@ -2,5 +2,5 @@ Blockly.Msg.LINE_V1 = '值 1 :';
 Blockly.Msg.LINE_V2 = '值 2 :';
 Blockly.Msg.LINE_V3 = '值 3 :';
 Blockly.Msg.LINE_MSG = '訊息 :';
-Blockly.Msg.LINE_STKVER = '表情貼圖主題';
-Blockly.Msg.LINE_STKID = ' 貼圖代號';
+Blockly.Msg.LINE_STKVER = '表情主題';
+Blockly.Msg.LINE_STKID = ' 表情代號';
